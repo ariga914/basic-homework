90c7a9577e454ef59bf6e0f77ae3443578e27727
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-04-21 18:08:30 +0900

    added php directory

---
+++ php/homework2.php
@@ -0,0 +1,15 @@
+<?php 
+//homework2
+function factorialOfNumber ($n){
+    $i = 1;
+    $product = 1;
+    while ($i < $n) {
+        $product *= $i;
+        ++$i;
+    }
+    return $product;
+}
+
+echo factorialOfNumber (10);
+
+?>
