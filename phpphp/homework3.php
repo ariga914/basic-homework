bcdcce181cc4ed82f741e84c557c3ff2f1ff8c70
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-04-22 14:25:50 +0900

    standardize 4space-indent

--- phpphp/homework3.php
+++ phpphp/homework3.php
@@ -1,19 +1,19 @@
 <?php
 // homework3
 function checkPrimeNumber ($n) {
-  if ($n === 0 || $n === 1)  {
-    return false;
-  }
-  if ($n === 2) {
-    return true;
-  } 
-  $m = $n/2;
-  for($i = 2; $i <= $m; ++$i) {
-    if ($n%$i === 0) {
-      return false;  
+    if ($n === 0 || $n === 1)  {
+        return false;
     }
-  }
-  return true;
+    if ($n === 2) {
+        return true;
+    } 
+    $m = $n/2;
+    for($i = 2; $i <= $m; ++$i) {
+        if ($n%$i === 0) {
+            return false;  
+        }
+    }
+    return true;
 }
 
 ?>
--- phpphp/homework4.php
+++ phpphp/homework4.php
@@ -1,12 +1,12 @@
 <?php
 function checkPrimeNumber2 ($n) {    
-  for ($i = 0; $i <= $n; ++$i) {
-    $a = checkPrimeNumber($i);
-    if ($a == 1) {
-      echo $i;
-      echo "<br>";  
-    }    
-  }  
+    for ($i = 0; $i <= $n; ++$i) {
+        $a = checkPrimeNumber($i);
+        if ($a == 1) {
+            echo $i;
+            echo "<br>";  
+        }    
+    }  
 }
 
 ?>
