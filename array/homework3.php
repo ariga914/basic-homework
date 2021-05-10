6bcc40b6a42371afb5efe362c9292102b7f063ed
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-10 11:12:40 +0900

    changed into using only one loop

--- array/homework3.php
+++ array/homework3.php
@@ -6,44 +6,47 @@ $multiArray =array(
 );
 
 $sum = 0;
+$avg = 0;
+$min = 100;
+$max = 0;
+
 for ($i=0; $i<sizeof($multiArray); ++$i) {
     $subSum = 0;
+    $subMin = 100;
+    $subMax = 0;
+
     for ($j=0; $j<sizeof($multiArray[$i]); ++$j) {
         $subSum += $multiArray[$i][$j];
+
+        if ($multiArray[$i][$j] < $subMin){
+            $subMin = $multiArray[$i][$j];
+        }
+
+        if ($multiArray[$i][$j] > $subMax){
+            $subMax = $multiArray[$i][$j];
+        }
     } 
+
     $sum += $subSum;
+
+    if ($subMin < $min) {
+        $min = $subMin;
+    }
+
+    if ($subMax > $max) {
+        $max = $subMax;
+    }
 }
+
+
+
 echo $sum."<br>";
 
 $avg = $sum / (sizeof($multiArray) * sizeof($multiArray[0]));
 echo round($avg)."<br>";
 
-$min = 100;
-$subMin = 100;
-for ($i=0; $i<sizeof($multiArray); ++$i) {
-    for ($j=0; $j<sizeof($multiArray[$i]); ++$j) {
-        if ($multiArray[$i][$j] < $subMin){
-            $subMin = $multiArray[$i][$j];
-        }
-    } 
-    if ($subMin < $min) {
-        $min = $subMin;
-    }
-}
 echo $min."<br>";
 
-$max = 0;
-$subMax = 0;
-for ($i=0; $i<sizeof($multiArray); ++$i) {
-    for ($j=0; $j<sizeof($multiArray[$i]); ++$j) {
-        if ($multiArray[$i][$j] > $subMax){
-            $subMax = $multiArray[$i][$j];
-        }
-    } 
-    if ($subMax > $max) {
-        $max = $subMax;
-    }
-}
 echo $max;
 
 ?>
