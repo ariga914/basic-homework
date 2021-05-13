8556f4f1b1203c7c943b7c8c864e7a523567e9f8
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-13 11:45:02 +0900

    first tackling

---
+++ heap_homework.php
@@ -0,0 +1,44 @@
+<?php
+function MaxHeapify(&$data, $heapSize, $index) {
+    $left = ($index + 1) * 2 - 1;
+    $right = ($index + 1) * 2;
+    $largest = 0;
+ 
+    if ($left < $heapSize && $data[$left] > $data[$index])
+        $largest = $left;
+    else
+        $largest = $index;
+ 
+    if ($right < $heapSize && $data[$right] > $data[$largest])
+        $largest = $right;
+ 
+    if ($largest != $index) {
+        $temp = $data[$index];
+        $data[$index] = $data[$largest];
+        $data[$largest] = $temp;
+ 
+        MaxHeapify($data, $heapSize, $largest);
+    }
+}
+ 
+ function HeapOut(&$data, $count, $k) {
+    $heapSize = $count;
+    $printer = [];
+    for ($p = ($heapSize - 1) / 2; $p >= 0; $p--) 
+        MaxHeapify($data, $heapSize, $p);
+ 
+    for ($i = $count - 1; $i > $count-1-$k; $i--)
+    {
+        $temp = $data[$i];
+        array_push($printer, $data[0]);
+        $data[0] = $temp;
+ 
+        $heapSize--;
+        MaxHeapify($data, $heapSize, 0);
+    }
+    print_r($printer);
+}
+ 
+$array = array(20,43,65,88,11,33,56,74);
+HeapOut($array,8, 3);
+?>
