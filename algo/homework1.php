2389b9d3e6ee234e061b2cfa15c76bb9010fb7bd
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-17 14:25:26 +0900

    fixed an incorrect indent style

--- algo/homework1.php
+++ algo/homework1.php
@@ -3,12 +3,12 @@ function partition(&$array, $left, $right) {
     $pivot = $array[$right];
     $i = $left -1;
     for ($j = $left; $j < $right; $j++) {
-          if(($array[$j] < $pivot)){
+        if(($array[$j] < $pivot)){
             $i++;
             $temp = $array[$i];
             $array[$i] = $array[$j];
             $array[$j] = $temp;
-          }
+        }
     }
     $temp = $array[$i + 1];
     $array[$i + 1] = $array[$right];
