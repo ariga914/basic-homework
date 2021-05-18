75767e3d939158fb4c9e977e40a6c98882c56b7d
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-18 17:50:23 +0900

    rewrite following hints

--- algo/homework2.php
+++ algo/homework2.php
@@ -1,23 +1,38 @@
 <?php
 require_once('homework1.php');
 
-function tripleSort (array $a, array $b, int $k) {
-    $c = array();
-    for ($i=0; $i<sizeof($a); $i++) {
-        for ($j=0; $j<sizeof($b); $j++) {
-            array_push($c, [$a[$i],$b[$j]]);
-        }
+function findSmallest (array $a, array $b, int $k) {
+    if ($k > count($a)*count($b)) {
+        echo "{$k} pairs does not exist";
+        return;
     }
     
-    quicksort($c, 0, sizeof($c)-1);
+    quicksort($a, 0, sizeof($a)-1);
+    quicksort($b, 0, sizeof($b)-1);
     
-    for($i=0; $i<$k; $i++) {
-        print_r($c[$i]);
+    $index2 = array_fill(0, count($a), 0);
+
+    while ($k > 0) {
+        $minSum = PHP_INT_MAX;
+        $minIndex = 0;
+
+        foreach ($a as $i => $value1) {
+            if ($index2[$i] < count($b) && ($value1 + $b[$index2[$i]] < $minSum)) {
+                $minIndex = $i;
+                $minSum = $value1 + $b[$index2[$i]];
+            }
+        }
+
+        echo "[{$a[$minIndex]}, {$b[$index2[$minIndex]]}]";
+
+        $index2[$minIndex]++;
+
+        $k--;
     }
 }   
 
 $array1 = [11, 7, 1];
-$array2 = [4, 6, 2];
+$array2 = [4, 9, 2];
 $k = 3;
-tripleSort($array1, $array2, $k);
+findSmallest($array1, $array2, $k);
 ?>
