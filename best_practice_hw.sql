7abdd64416c73f3f014a2eca59d0c14c7d4a989f
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-27 12:31:47 +0900

    used uppercase & alias

--- best_practice_hw.sql
+++ best_practice_hw.sql
@@ -3,17 +3,17 @@
 * Purpose:SQL best practice training
 */
 
-select 
+SELECT 
     agents.WORKING_AREA,   
     avg(agents.COMISSION) AS `commission_average`,
     count(agents.AGENT_NAME) AS `number_of_agents`
-from
+FROM 
     agents
-group by 
+GROUP BY 
 	-- get the average 'commission' and the number of agents for each group of 'working_area' 
     agents.WORKING_AREA 
-having 
+HAVING 
 	--condition: number of agents for each group of 'working_area' must be less than 3
     count(agents.AGENT_NAME)< 3
-order by 
-    avg(agents.COMISSION), count(agents.AGENT_NAME) desc;
+ORDER BY 
+    `commission_average`, `number_of_agents` desc;
