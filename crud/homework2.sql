684c8d7f9bce5c797e2974ef1b43f54eec65ad65
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-27 14:46:27 +0900

    maybe this is checkmate

--- crud/homework2.sql
+++ crud/homework2.sql
@@ -1,11 +1,7 @@
 UPDATE agents
 SET agents.COMISSION = agents.COMISSION - 0.02
-WHERE agents.AGENT_CODE = 
+WHERE agent_code IN
 (
-    SELECT customer.AGENT_CODE
-    FROM customer
-    GROUP BY customer.AGENT_CODE
-    ORDER BY SUM(customer.PAYMENT_AMT)
-    LIMIT 1
-)
-;
+SELECT agent_code FROM customer
+WHERE PAYMENT_AMT=(SELECT MIN(PAYMENT_AMT) FROM customer)
+);
