af9f17f27b549af32a3b164bcb0ee8e5a005b393
ariga914 <nobuyuki.ariga@g.pascalia.co.jp>
2021-05-30 19:24:06 +0900

    first submission

---
+++ indices/index.js
@@ -0,0 +1,44 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+/*
+href="javascript:void(0);" is a trick to prevent to page jump to
+top if you're set href="#" and click on its tag.
+its means attribute href will not works when <a> tag was clicked.
+Because result of javascript:void(0) return false;
+*/
+
+function toggleScrollButton() {
+    const lastPosition = window.scrollY;
+    const scrollBtn = document.getElementById('scroll');
+
+    if (lastPosition !== 0) {
+        scrollBtn.setAttribute('style', 'display: block;');
+    } else {
+        scrollBtn.setAttribute('style', 'display: none;');
+    }
+}
+
+window.addEventListener('scroll', function() {
+    toggleScrollButton();
+});
+
+window.addEventListener('load', () => {
+    toggleScrollButton();
+});
+
+function showLoginForm() {
+    let state = document.getElementById("login-form");
+    let cssStyleDeclaration = getComputedStyle(state, null);
+    if (cssStyleDeclaration.getPropertyValue("display") === "none") {
+        state.style.display = "inline-block";
+    }
+}
+
+function hideLoginForm() {
+    let state = document.getElementById("login-form");
+    let cssStyleDeclaration = getComputedStyle(state, null);
+    if (cssStyleDeclaration.getPropertyValue("display") !== "none") {
+        state.style.display = "none";
+    }
+}
